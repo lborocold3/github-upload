f3d6229ec6da423e9e1b12eeac2340d1977562bb
Dong <L20565@u-ssi.net>
2020-03-06 17:29:18 +0000

    go

--- python/.vscode/oracleTest.py
+++ python/.vscode/oracleTest.py
@@ -3,9 +3,11 @@
 from __future__ import print_function
 
 import cx_Oracle
+import db_config
 
 # Connect as user "hr" with password "welcome" to the "orclpdb1" service running on this computer.
-connection = cx_Oracle.connect("l20565", "", "AEPW04")
+connection = cx_Oracle.connect(db_config.user, db_config.pw, db_config.dsn)
+print("Database version:", connection.version)
 
 cursor = connection.cursor()
 cursor.execute("""
