794e3e1536e8a3dddba963e19a7af0fe5b3c762b
lborocold <leng.dong@outlook.com>
2020-08-19 17:06:26 +0100

    RNILF04

--- python/.vscode/Credit_Checking_Tender/main.py
+++ python/.vscode/Credit_Checking_Tender/main.py
@@ -5,28 +5,34 @@ from matplotlib import pyplot
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import f1_score
 from sklearn.metrics import auc
-
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 import pandas as pd
 
 #%%
-df=pd.read_csv(r"C:\Users\L20565\OneDrive - E.ON\Sample_Data_RNOLF.csv")
-bad_Flag = df.loc[:, "Bad_Flag_2"]
-RNOLF04=df.loc[:,"RNOLF04_inverse"]
-RNOLN01=df.loc[:,"RNOLN01_inverse"]
-#print(label)
+df=pd.read_csv(r"C:\Users\L20565\OneDrive - E.ON\CRD_CHK_RETRO_REF_S6.csv")
+EQF_RNILF04 = df.loc[:, ["BAD_FLAG","BAD_FLAG_365","EQF.RNILF04"]]
+EQF_RNILF04['EQF.RNILF04'].replace('', np.nan, inplace=True)
+EQF_RNILF04.dropna(subset=['EQF.RNILF04'], inplace=True)
+
+# %%
+scaler = MinMaxScaler()
+EQF_RNILF04_minmax=scaler.fit_transform(EQF_RNILF04)
+EQF_RNILF04_df[3]=1-EQF_RNILF04_df[2]
+
 
 #%%
-RNOLF04_rocauc = roc_auc_score(bad_Flag, RNOLF04)
-RNOLN01_rocauc = roc_auc_score(bad_Flag, RNOLN01)
+rocauc = roc_auc_score(EQF_RNILF04_df[0], EQF_RNILF04_df[3])
+#ENOLF_rocauc = roc_auc_score(bad_Flag, ENOLF)
 #print('RNOLF04: ROC AUC=%.3f' % (RNOLF04_rocauc))
-#print('RNOLN01: ROC AUC=%.3f' % (RNOLN01_rocauc))
+#print('ENOLF: ROC AUC=%.3f' % (ENOLF_rocauc))
 # %%
-RNOLF04_fpr, RNOLF04_tpr,_= roc_curve(bad_Flag, RNOLF04)
-RNOLN01_fpr, RNOLN01_tpr,_ = roc_curve(bad_Flag, RNOLN01)
+fpr, tpr,_= roc_curve(EQF_RNILF04_df[0], EQF_RNILF04_df[3])
+#ENOLF_fpr, ENOLF_tpr,_ = roc_curve(bad_Flag, ENOLF)
 
 # plot the roc curve for the model
-pyplot.plot(RNOLF04_fpr, RNOLF04_tpr, linestyle='-', label='RNOLF04')
-pyplot.plot(RNOLN01_fpr, RNOLN01_tpr, linestyle='-', label='RNOLN01')
+#pyplot.plot(RNOLF04_fpr, RNOLF04_tpr, linestyle='-', label='RNOLF04')
+pyplot.plot(fpr, tpr, linestyle='-', label='RNILF04')
 # axis labels
 pyplot.xlabel('False Positive Rate')
 pyplot.ylabel('True Positive Rate')
@@ -34,21 +40,21 @@ pyplot.ylabel('True Positive Rate')
 pyplot.legend()
 # show the plot
 pyplot.show()
-print('RNOLF04: ROC AUC=%.3f' % (RNOLF04_rocauc))
-print('RNOLN01: ROC AUC=%.3f' % (RNOLN01_rocauc))
+#print('RNOLF04: ROC AUC=%.3f' % (RNOLF04_rocauc))
+print('RNILF04: ROC AUC=%.3f' % (rocauc))
 # %%
 RNOLF04_precision, RNOLF04_recall, _ = precision_recall_curve(bad_Flag, RNOLF04)
 RNOLF04_auc = auc(RNOLF04_recall, RNOLF04_precision)
 
-RNOLN01_precision, RNOLN01_recall, _ = precision_recall_curve(bad_Flag, RNOLN01)
-RNOLN01_auc =  auc(RNOLN01_recall, RNOLN01_precision)
+ENOLF_precision, ENOLF_recall, _ = precision_recall_curve(bad_Flag, ENOLF)
+ENOLF_auc =  auc(ENOLF_recall, ENOLF_precision)
 # %%
 # summarize scores
 #print('Logistic: f1=%.3f auc=%.3f' % (lr_f1, lr_auc))
 # plot the precision-recall curves
 #no_skill = len(testy[testy==1]) / len(testy)
 #pyplot.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
-pyplot.plot(RNOLN01_recall, RNOLN01_precision, linestyle='-', label='RNOLN01')
+pyplot.plot(ENOLF_recall, ENOLF_precision, linestyle='-', label='ENOLF')
 pyplot.plot(RNOLF04_recall, RNOLF04_precision, linestyle='-', label='RNOLF04')
 # axis labels
 pyplot.xlabel('Recall')
@@ -58,5 +64,5 @@ pyplot.legend()
 # show the plot
 pyplot.show()
 print('RNOLF04:  prAUC=%.3f' % (RNOLF04_auc))
-print('RNOLN01:  prAUC=%.3f' % (RNOLN01_auc))
+print('ENOLF:  prAUC=%.3f' % (ENOLF_auc))
 # %%
